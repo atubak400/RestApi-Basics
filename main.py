75a56d5657f45b639baa778fd85d33390669a8da
Kingsley Atuba <atubak40@gmail.com>
2022-06-21 15:28:31 +0100

    implement PUT and GET and throwing error if video id doesn't exist

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video is required")
-video_put_args.add_argument("views", type=int, help="Views of the video")
-video_put_args.add_argument("likes", type=int, help="Likes on the video")
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
 videos = {}
+
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Could not find video...")
+
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
         args = video_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return videos[video_id], 201
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,5 +2,11 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
+response = requests.put(BASE + "video/1", {"likes": 10, "name": "Kingsley", "views": 10000})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
+print(response.json())
+input()
+response = requests.get(BASE + "video/6")
 print(response.json())
