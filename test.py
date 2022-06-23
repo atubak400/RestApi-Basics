3fdf067a945f09db07c3926129c4f70b4fd6a532
Kingsley Atuba <atubak40@gmail.com>
2022-06-23 09:34:14 +0100

    validate request and handling delete requests

--- test.py
+++ test.py
@@ -2,10 +2,20 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "Kingsley", "views": 10000})
-print(response.json())
+data = [
+    {"likes": "78", "name": "Peters video", "views": 60000},
+    {"likes": "56", "name": "Dans video", "views": 100000},
+    {"likes": "64", "name": "Kingsleys video", "views": 910000}
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+response = requests.delete(BASE + "video/0")
+print(response)
 input()
-response = requests.get(BASE + "video/1")
+response = requests.get(BASE + "video/2")
 print(response.json())
 input()
 response = requests.get(BASE + "video/6")
